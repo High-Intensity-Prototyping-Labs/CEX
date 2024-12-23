fda7ead576ad9bb71f0aff3b557d3b853784a3d6
amellalalex <github@alexamellal.com>
2024-12-23 11:21:32 -0400

    Moved to http::response object
    This one is the minimal heap-pointer, size and string-conversion

--- cex/http.hpp
+++ cex/http.hpp
@@ -2,6 +2,7 @@
 #define CEX_HTTP
 
 // Standard C Dependencies
+#include <cstddef>
 #include <cstdlib>
 #include <cstdint>
 #include <cassert>
@@ -29,11 +30,19 @@ namespace cex {
     }
 
     namespace http {
-        std::tuple<std::string, cex::error> get(std::string url) {
+        struct response {
+            void*   data_buffer_heap;
+            size_t  data_len;
+            std::string body(void) {
+                return std::string((char*)data_buffer_heap, data_len);
+            }
+        };
+
+        std::tuple<http::response*, cex::error> get(std::string url) {
             CURL    *curl;
             CURLcode res;
 
-            char    *data;
+            http::response *response = new http::response;
 
             curl = curl_easy_init();
 
@@ -52,18 +61,18 @@ namespace cex {
                         return size * nmemb;
                     }
                 );
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response->data_buffer_heap);
 
                 // Perform request
                 res = curl_easy_perform(curl);
 
                 // C++11
                 if(res != CURLE_OK) {
-                    return std::make_tuple("", errors::New(res));
+                    return std::make_tuple(nullptr, errors::New(res));
                 }
 
                 // Get cURL internals into response
-                // curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &response->data_len);
+                curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &response->data_len);
                 // curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &response->effective_url); // WARNING: Will be lost
                 // curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response->code);
                 // curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &response->time);
@@ -73,11 +82,11 @@ namespace cex {
                 // curl_global_cleanup();
 
                 // OK
-                return std::make_tuple(std::string(data), nullptr);
+                return std::make_tuple(response, nullptr); // WHAT does .get() do??
             }
 
             // FAIL
-            return std::make_tuple("", errors::New(res));
+            return std::make_tuple(nullptr, errors::New(res));
         }
     }
 }
